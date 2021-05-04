33b747d781c5b39f49a41fc0298afabb68a68838
Poison <35471036+coolcoy12@users.noreply.github.com>
2021-05-04 08:36:57 -0500

    Update particles.hpp
    fixed some problems and added gravity

--- particle/particles.hpp
+++ particle/particles.hpp
@@ -12,28 +12,31 @@ struct Particle
     Texture2D texture;
     Vector2 position;
     Color color;
-    float Size;
-    int Speed;
-    int Angle;
-    int LifeTime;
+    float Size = 0.0f;
+    int Speed = 0;
+    int Angle = 0;
+    int LifeTime = 0;
+    int Gravity = 0;
 };
-//Vector2 position; Color color; int size; int Speed; int Angle; int LifeTime;
+//Vector2 position; Color color; int size; int Speed; int Angle; int LifeTime; int Gravity;
 struct PixelParticle 
 {
-    Vector2 position;
-    Color color;
-    int Size;
-    int Speed;
-    int Angle;
-    int LifeTime;
+    Vector2 Position;
+    Color Color;
+    int Size = 0;
+    int Speed = 0;
+    int Angle = 0;
+    int LifeTime = 0;
+    int Gravity = 0;
+    int ID = 0;
 };
 
 std::vector<PixelParticle*> PixelParticles;
 std::vector<Particle*> Particles;
 
 //Used to Draw Particle
-namespace Poison
-{
+    
+
     void DrawParticle(Texture2D Texture, Vector2 ParticlePosition, int Angle, int Speed, Color color)
     {
         ParticlePosition.x += (cos(Angle * PI /180)* Speed);
@@ -50,25 +53,25 @@ namespace Poison
     {
         Particle.position.x += (cos(Particle.Angle * PI /180)* Particle.Speed);
         Particle.position.y += (sin(Particle.Angle * PI /180)* Particle.Speed);
-        Particle.LifeTime--;
-        if(Particle.LifeTime >= 0)
-            DrawTextureEx(Particle.texture, Particle.position, 0, Particle.Size, Particle.color);
+        Particle.position.y += Particle.Gravity * GetFrameTime();
+        Particle.LifeTime - GetFrameTime();
+        DrawTextureEx(Particle.texture, Particle.position, 0, Particle.Size, Particle.color);
     }
     //Place Between StartDraw() and EndDraw()
     void AutoPixelParticle(PixelParticle &Particle)
     {
-        Particle.position.x += (cos(Particle.Angle * PI /180)* Particle.Speed);
-        Particle.position.y += (sin(Particle.Angle * PI /180)* Particle.Speed);
-        Particle.LifeTime--;
-        if(Particle.LifeTime >= 0)
-            DrawRectangleV(Particle.position, Vector2{(float)Particle.Size, (float)Particle.Size}, Particle.color);      
+        Particle.Position.x += (cos(Particle.Angle * PI /180)* Particle.Speed);
+        Particle.Position.y += (sin(Particle.Angle * PI /180)* Particle.Speed);
+        Particle.Position.y += Particle.Gravity * GetFrameTime();
+        Particle.LifeTime - GetFrameTime();
+        DrawRectangleV(Particle.Position, Vector2{(float)Particle.Size, (float)Particle.Size}, Particle.Color);      
     }
     Color REffect(int x, int y, int z)
     {
         Color colorEffect = ColorFromHSV((float)(((x + y + z)*18)%360), 0.75f, 0.9f);
         return colorEffect;
     }
-}
+
 void ParticleClear()
 {
     for (int i = Particles.size() - 1; i >= 0; i--)
@@ -88,3 +91,12 @@ void ParticleClear()
         }
     }   
 }
+/*bool CheckCollisionParticles(PixelParticle* A, PixelParticle* B)
+{
+    bool C = false;
+    if (CheckCollisionRecs(Rectangle{ (float)A->Position.x, (float)A->Position.y, (float)A->Size, (float)A->Size }, Rectangle{ (float)B->Position.x, (float)B->Position.y, (float)B->Size, (float)B->Size }))
+    {
+        C = true;
+    }
+    return C;
+}*/
